df28f73c203aca4848fe9cb2e59669c8bd05a047
Dag Øyvind Tornes <dagtornes@gmail.com>
2014-05-01 17:44:11 +0200

    Added CORS compat (allow all right now..)

--- backend/pro2backend/pro2backend/settings.py
+++ backend/pro2backend/pro2backend/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'caseService',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,6 +55,7 @@ ROOT_URLCONF = 'pro2backend.urls'
 
 WSGI_APPLICATION = 'pro2backend.wsgi.application'
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
