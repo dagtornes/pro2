98250ae076c5b25c7d92edd764bd01e835cef00a
Dag Øyvind Tornes <dagtornes@gmail.com>
2014-05-05 14:32:13 +0200

    Expose user extras to admin interface

--- backend/pro2backend/caseService/admin.py
+++ backend/pro2backend/caseService/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-from caseService.models import Case, ProcessStep
+from caseService.models import Case, ProcessStep, UserExtras
 
-# Register your models here.
+
+class UserExtrasInline(admin.StackedInline):
+    model = UserExtras
+    can_delete = False
+
+class UserAdmin(UserAdmin):
+    inlines = (UserExtrasInline,)
+
+
 admin.site.register(Case)
 admin.site.register(ProcessStep)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
