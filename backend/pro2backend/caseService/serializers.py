82a429152abe97785a9f4c64f36285651d256b2e
Dag Øyvind Tornes <dagtornes@gmail.com>
2014-05-05 16:18:56 +0200

    Expose users name via API

--- backend/pro2backend/caseService/serializers.py
+++ backend/pro2backend/caseService/serializers.py
@@ -26,4 +26,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'cases', 'extra')
+        fields = ('id', 'username', 'first_name', 'last_name', 'cases', 'extra')
