ecbbc817c627df70c5fb042ec68614dd15538173
Dag Øyvind Tornes <dag.oyvind.tornes@acando.com>
2014-05-07 16:31:14 +0200

    Add person to case in BE

--- backend/pro2backend/caseService/serializers.py
+++ backend/pro2backend/caseService/serializers.py
@@ -3,22 +3,26 @@ from rest_framework import serializers
 from caseService.models import Case, ProcessStep
 from caseService.models import UserExtras
 
+
 class CaseSerializer(serializers.ModelSerializer):
     owner = serializers.Field(source='owner.username')
 
     class Meta:
         model = Case
-        fields = ('id', 'created', 'owner', 'step')
+        fields = ('id', 'created', 'owner', 'step', 'person')
+
 
 class ProcessStepSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProcessStep
         fields = read_only_fields = ('id', 'name', 'default_transition')
 
+
 class UserExtrasSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserExtras
         fields = ('region', 'office')
+
 
 class UserSerializer(serializers.ModelSerializer):
     cases = serializers.PrimaryKeyRelatedField(many=True)
